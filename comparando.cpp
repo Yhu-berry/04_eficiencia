a726b9f6cdd40c489007f05f820b96183a217fdf
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-10-04 17:34:40 -0300

    Se agrego la funcion rand() para generar numeros aleatorios

---
+++ comparando.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Funcion shellsort 
+int shellsort(int A[], int n);
+
+int main(){
+    int num, Arr[100000];   
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> num;
+
+    // Funcion que genera elementos aleatoriamente
+    srand(time(0));
+    for (int i = 0; i < num; i++) {
+        Arr[i] = rand() % 1000;  // Números aleatorios entre 0 y 999
+    }
+
+    // Mostrar elementos generados
+    cout << "Elementos generados: ";
+    for (int i = 0; i < num; i++) {
+        cout << Arr[i] << " ";
+    }
+    cout << endl << endl;
+    
+    return 0;
+}
+
+// Implementacion de shellsort
+int shellsort(int A[], int n) {
+    for (int k = n / 2; k > 0; k /= 2) {
+        for (int i = k; i < n; i++) {
+            int aux = A[i];
+            int j;
+            for (j = i; j >= k && A[j - k] > aux; j -= k) {
+                A[j] = A[j - k];
+            }
+            A[j] = aux;
+        }
+    }
+    return 0;
+}
