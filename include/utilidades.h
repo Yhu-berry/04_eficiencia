2ea1d6d9d15201c1f638646546878d5680901222
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-10-22 19:10:07 -0300

    Agregango una variable externa para el uso de las funciones

--- include/utilidades.h
+++ include/utilidades.h
@@ -3,10 +3,15 @@
 #define UTILIDADES_H
 
 //Generar un arreglo aleatorio
+void generarArreglo(int arr[],int n);
 
 //Copiar el tamaño del arreglo para que todos posean el mismo
-
+void copiarArr(int Arr_crlV[], const int Arr_crlC[], int n);
 //Funcion para imprimir el arreglo
+void imprimirArr(const int arr[], int n);
 
+//Designar el tamaño maximo del arreglo
+const int n_Max=1000000;
+extern int arrAleatorio[n_Max];
 
 #endif
