9159f45d6360aec5fe5040ba3247c90155452bcd
Jhonel <jgapazap@unjbg.edu.pe>
2024-10-24 10:25:12 -0500

    Prueba de algoritmos terminada

--- PRUEBA/utilidades.h
+++ PRUEBA/utilidades.h
@@ -1,7 +1,6 @@
 #ifndef UTILIDADES_H
-
 #define UTILIDADES_H
-
+#include "busqueda.h"
 //Generar un arreglo aleatorio
 void generarArreglo(int* arr,int n);
 
@@ -12,5 +11,11 @@ void imprimirArr(const int* arr, int n);
 
 //Designar el tamaño maximo del arreglo
 const int n_Max=1000000;
+
+void mostrarArreglo(int a[], int n);
+
+void imprimirComparacion(Metodos tiempo[], Metodos promedio[], int n, int c, int datos, int busquedas, int elementoBuscar[], int r[]);
+
+void mezclarArreglo(int* arreglo, int tamano);
 
 #endif
